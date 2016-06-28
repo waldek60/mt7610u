10810919da4e31296eaa2549b634a25fb9ea06cd
Tristian Celestin <tristian.celestin@outlook.com>
2016-06-27 22:33:33 -0400

    Fixed links in journal. Changed theme?

--- journal/pelicanconf.py
+++ journal/pelicanconf.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 AUTHOR = 'Tristian Celestin'
 SITENAME = 'mt7610u Project Journal'
 SITEURL = ''
+THEME = 'notmyidea'
 
 PATH = 'content'
 
@@ -21,12 +22,7 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 DEFAULT_PAGINATION = False
 
