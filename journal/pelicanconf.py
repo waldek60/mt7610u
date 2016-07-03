edff4c0432e89e74f76213519cb2f3bbb8a75d74
Tristian Celestin <tristian.celestin@outlook.com>
2016-07-02 21:15:26 -0400

    Removed useless links

--- journal/pelicanconf.py
+++ journal/pelicanconf.py
@@ -21,12 +21,7 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 DEFAULT_PAGINATION = 10
 
